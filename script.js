6bd0b8056dc67a67bef70cb26596909ec3914172
BeataBarbara <skorusbeata@gmail.com>
2020-01-30 12:48:23 +0100

    send page

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-window.onload = function losujCytat(polecenie,liczba) {
+window.onload = function losujCytat() {
     var x = document.getElementById("cytaty").childElementCount; 
     var liczba = Math.ceil(Math.random()*x)
-      var odp = document.getElementById('cytat_' + liczba);
-        odp.classList.remove("hide")
-       };
-     function refresh() {
-      location.reload()
+    for (i=0; i<x; i++) {
+document.getElementsByTagName("q")[i].classList.remove("show")     
+    }
+      var odp = document.getElementsByTagName("q")[liczba-1];
+        odp.classList.add("show")
+       }
+
+ 
+      function refresh() {
+         var x = document.getElementById("cytaty").childElementCount; 
+         var liczba = Math.ceil(Math.random()*x)
+         for (i=0; i<x; i++) {
+     document.getElementsByTagName("q")[i].classList.remove("show")     
+         }
+           var odp = document.getElementsByTagName("q")[liczba-1];
+             odp.classList.add("show")
      }
 
-function love () {
-    var x = document.getElementById("cytaty").childElementCount; 
-    for (i=1; i<=x; i++) {
-        elem = document.getElementById("cytat_", i)
-        if (elem.getAttribute("class") == "hide") {
-            alert("abbbb")
-        } else {
-            alert("abrfggvkfjs")
+function drawLots (kryt) { 
+   var kryt=kryt.getAttribute("id")
+        console.log(kryt)
+        var x = document.getElementById("cytaty").childElementCount; 
+        const table = []
+           for (i=0; i<x; i++) { 
+       var elem = document.getElementsByTagName("q")[i]
+            var elem2 = elem.classList.contains(kryt)
+               if (elem2 == true) {table.push(elem.textContent)}    }
+            var liczba = Math.ceil(Math.random()*(table.length)-1)
+            var odp = table[liczba];
+            var newElem = document.createElement("q")
+       document.getElementById("cytaty").appendChild(newElem)
+        newElem.innerHTML = odp
+        var x = document.getElementById("cytaty").childElementCount; 
+        for (i=0; i<x; i++) {
+    document.getElementsByTagName("q")[i].classList.remove("show")     
         }
-    } }
+               newElem.classList.add("show")
+            }
 
 
